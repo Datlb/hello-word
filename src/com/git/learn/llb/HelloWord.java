5b91d56e454af612fbf1f7382a3a873fe9d14988
Datlb <liulb520@sina.cn>
2016-10-18 11:18:13 +0800

    hello word

---
+++ src/com/git/learn/llb/HelloWord.java
@@ -0,0 +1,13 @@
+/** 
+* @author liulb
+* @time Oct 17, 2016, 5:01:20 PM
+* @Description 
+*/
+package com.git.learn.llb;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word!");
+	}
+}
